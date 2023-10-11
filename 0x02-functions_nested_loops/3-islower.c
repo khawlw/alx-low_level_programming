4eed17f789b13eea8ef3481842bba47cf3534283
khawlw <khawla.anoui@gmail.com>
2023-10-11 11:49:20 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - cheks for lowercase character
  * @c: The character to be checked
+ * Description: chek for lowercase and return to 1 if c is lowercase or 0 if otherwise
  * Return 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)

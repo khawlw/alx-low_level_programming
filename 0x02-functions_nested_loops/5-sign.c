2e031a7bacd5aafa43bc09c2b7c48114eeb67ecd
khawlw <khawla.anoui@gmail.com>
2023-10-11 12:29:40 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@ nclude "main.h"
  * @c: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
-int print-sign(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{

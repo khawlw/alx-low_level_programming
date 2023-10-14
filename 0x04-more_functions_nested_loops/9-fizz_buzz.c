4174a23638a1c746850f6c5f566df5da752baee5
khawlw <khawla.anoui@gmail.com>
2023-10-13 18:46:22 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizBuzz");
+			printf("FizzBuzz");
 		else
 			printf("%d", num);
 		if (num != 100)

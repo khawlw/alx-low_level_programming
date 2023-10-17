29395fdf97080bcfd418970736a470a8b540d285
khawlw <khawla.anoui@gmail.com>
2023-10-16 18:25:35 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,14 +14,14 @@ void rev_string(char *s)
 	char temp;
 
 	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++1)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
 
 	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[l - 1 - i]; /*- 1 because the array starts from 0*/
-		s[l - 1 - i] =  temp;
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l - 1 - i] = temp;
 	}
 }

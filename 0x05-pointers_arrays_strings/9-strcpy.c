b2516d01230e51ff8665addaf694b47c241e2740
khawlw <khawla.anoui@gmail.com>
2023-10-17 14:12:07 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,9 +4,9 @@
  * *_strcpy - copies the string pointed to by src
  * @dest: char type string
  * @src: char type string
- * Description: Copy the string pointed to by pointer 'src' to
+ * Descriptioni: Copy the string pointed to by pointer 'src' to
  * the buffer pointed to by 'dest'
- * Return; Pointer to 'dest'
+ * Return: Pointer to 'dest'
  */
 
 char *_strcpy(char *dest, char *src)

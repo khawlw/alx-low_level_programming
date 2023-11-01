23cf7aae04ab14d987a24d547b8be7f524dbfbe2
khawlw <khawla.anoui@gmail.com>
2023-11-01 14:56:20 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	{
 		for (i = 0; i < height; i++)
 		{
-			tab[i] = malloc(sizeof(**tab) * widtg);
+			tab[i] = malloc(sizeof(**tab) * width);
 			if (tab[i] == 0)
 			{
 				/*Free everything if malloc fails*/

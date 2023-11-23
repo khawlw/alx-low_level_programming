584380492e4bcad5c5e1461aa1e78be18bb75beb
khawlw <khawla.anoui@gmail.com>
2023-11-23 14:28:55 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,12 +14,12 @@ void print_binary(unsigned long int n)
 	{
 		if (n & 1L << --bit)
 		{
-			_putchar('l');
+			_putchar('1');
 			printed++;
 		}
 		else if (printed)
-			_putchar('0);
+			_putchar('0');
 	}
 	if (!printed)
-		_putchar('0);
+		_putchar('0');
 }

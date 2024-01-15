9b88820beed427aa94e1a42094af62177e5f1951
khawlw <khawla.anoui@gmail.com>
2024-01-15 12:59:29 -0800

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc -fpic -c *.c
-gcc -shared *.o -o liball.so
+gcc -Wall -Wextra -Werror -pedantic -c -fpic 0-main.c 1-create_dynamic_lib.c 5-strstr.c
+gcc -Wall -Wextra -Werror -pedantic -c -fpic 0-main.c 1-create_dynamic_lib.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
